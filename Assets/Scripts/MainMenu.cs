ab70f729fd2de8929cfeff7761f7ee8ce7704080
Damir <temirgalievdamir03@gmail.com>
2022-09-08 21:59:28 +0600

    Made Bullethell scene accessible from main menu scene

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -10,7 +10,7 @@ public class MainMenu : MonoBehaviour
     public void PlayGame()
     {
         pauseMenuUI.SetActive(false);
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene("BulletHell");
     }
 
     public void QuitGame()
